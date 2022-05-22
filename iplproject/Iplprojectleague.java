3c0234d2d840869850841b3917c8857c24c5d33b
SaiYoganandh Gumulapurapu <gsaiyoganandh.24895@gmail.com>
2022-05-22 17:38:39 +0530

    project tasks 4,5

--- iplproject/Iplprojectleague.java
+++ iplproject/Iplprojectleague.java
@@ -25,10 +25,15 @@ public class Iplprojectleague {
 		String deliveriesCsvFile ="/home/yoganandh24/Desktop/iplproject/deliveries.csv";
 		BufferedReader deliveriesCol=null;
 		List<Integer> matchIdsList = new ArrayList<>();
+		List<Integer> matchIdsList2015 = new ArrayList<>();
 		String deliveriesline="";
 		int skip2= 0;
 		Map<String,Integer> noOfExtras2016= new HashMap<>();	
-		
+		Map<String,Integer> bowlerRunsDetails = new HashMap<String,Integer>();
+		Map<String,Integer> bowlerBallsDetails = new HashMap<String,Integer>();
+		Map<String,Integer> batsmanDetails = new HashMap<String,Integer>();
+		Map<String,Double> bowlersEconomy = new HashMap<String,Double>();
+		Map<String,Integer> bowlerOvers = new HashMap<String,Integer>();
 		//###
 		//
 		try 
@@ -97,7 +102,16 @@ public class Iplprojectleague {
 					//System.out.println("60");
 				}
 				
+				
+				if(year .equals("2015")) {
+					//ol=ol+1;
+					matchIdsList2015.add(i1);
+					//System.out.println("60");
+				}
+				
 			}
+				
+			
 //task-3 and 4	
 			
 			//int w=0;
@@ -130,40 +144,75 @@ public class Iplprojectleague {
 							
 							
 						}
-					}
-					
-					
-					
-					
-					
-					
-					
+					}		
+				}
+///4th task		
+				int wide = Integer.parseInt(row1[10]);
+				int noBall = Integer.parseInt(row1[13]);
+				int batsManRuns = Integer.parseInt(row1[15]);
+				int byes = Integer.parseInt(row1[11]);
+				int legByes = Integer.parseInt(row1[12]);
+				int totalRuns =Integer.parseInt(row1[17]);
+				int penaltyRuns = Integer.parseInt(row1[14]);
+				int balls=0;
+				int runs =0;
+				
+				String bowlerName=row1[8];
+				String batsmanName=row1[6];
+				
+				if ((wide==0) && (noBall==0)){
+					balls=1;
+				}
+				if ((totalRuns !=0 ) && (byes==0) && (legByes == 0) && (penaltyRuns == 0)) {
+					runs=totalRuns;
 				}
 				
-				
+				for (Integer matchId1 :matchIdsList2015) {
+					int index1 = Integer.parseInt(row1[0]);
+					if(matchId1 == index1) {
+						if (bowlerRunsDetails.containsKey(bowlerName)) {
+							bowlerRunsDetails.put(bowlerName,bowlerRunsDetails.get(bowlerName)+runs);
+							
+						}
+						else {
+							bowlerRunsDetails.put(bowlerName,runs);
+						}
+						
+						if(bowlerBallsDetails.containsKey(bowlerName)) {
+							bowlerBallsDetails.put(bowlerName, bowlerBallsDetails.get(bowlerName)+balls);
+						}
+						else {
+							bowlerBallsDetails.put(bowlerName,balls);
+						}
+					}
+					
+				}
+				if(batsmanDetails.containsKey(batsmanName)) {
+					batsmanDetails.put(batsmanName,batsmanDetails.get(batsmanName)+batsManRuns);
+				}
+				else {
+					batsmanDetails.put(batsmanName, batsManRuns);
+				}
 				
 			}//end of 2nd while
 			
+////4th task--->
 			
+			for (String bowler :bowlerBallsDetails.keySet()) {
+				int totalballs = bowlerBallsDetails.get(bowler);
+				
+				if(totalballs>=6) {
+					bowlerOvers.put(bowler,bowlerBallsDetails.get(bowler)/6);
+				}
+				
+				
+			}
 			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+			double economy;
+			for (String bowler :bowlerOvers.keySet()) {
+				economy = bowlerRunsDetails.get(bowler)/bowlerOvers.get(bowler);
+				bowlersEconomy.put(bowler,economy);
+			}
 			
 			
 			
@@ -191,10 +240,10 @@ public class Iplprojectleague {
 		////////
 		System.out.println(noOfExtras2016);	
 		
-		
-		
-		
-		
+		//System.out.println(bowlerBallsDetails);
+		//System.out.println(bowlerRunsDetails);
+		System.out.println(bowlersEconomy);
+		System.out.println(batsmanDetails);
 		
 		
 		
